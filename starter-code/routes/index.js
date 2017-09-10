81f0a7694d62c9d01777222002d6aa2f308030d4
Jennie Dalgren <jennie.dalgren@gmail.com>
2017-09-10 16:33:31 +0200

    first commit, setting up the project

--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,3 +1,5 @@
+//this handles the standard things about the site.
+
 var express = require('express');
 var router = express.Router();
 
@@ -5,5 +7,27 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+//Check if user is valid to go to welcome or not (if logged in).
+router.use((req, res, next) => {
+  if (req.session.currentUser) {
+    next();
+  } else {
+
+    res.redirect('/login');
+  }
+});
+
+router.get("/logout", (req, res, next) => {
+  res.render("logout");
+});
+
+router.get("/home", (req, res, next) => {
+  res.render("home");
+});
+
+
+
+
 
 module.exports = router;
